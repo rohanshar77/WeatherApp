77f9749652f4445ff445e3501ae9fc5099ad6d0d
Saketh Jakka <sakethjakka@wireless-10-104-36-115.umd.edu>
2023-12-15 19:26:37 -0500

    added new buttons and incpmlete favs function

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const path = require("path");
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config({ path: path.resolve(__dirname, '.env') })
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const uri = process.env.MONGO_CONNECTION_STRING;
 
 try {
-
   // Connect to MongoDB
   mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log('Connected to WeatherApp!')
-
+  console.log("Connected to WeatherApp!");
 } catch (e) {
-  console.log(e)
+  console.log(e);
 }
 
 const app = express();
@@ -26,8 +24,8 @@ const port = process.env.PORT || 3000;
 
 // Set up middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // Define MongoDB Schema and Model (WeatherData)
 const weatherDataSchema = new mongoose.Schema({
@@ -37,50 +35,51 @@ const weatherDataSchema = new mongoose.Schema({
 });
 
 const locationSchema = new mongoose.Schema({
-  location: String
-})
+  location: String,
+});
 
-const Location = mongoose.model('favorites', locationSchema);
-const WeatherData = mongoose.model('WeatherData', weatherDataSchema);
+const Location = mongoose.model("favorites", locationSchema);
+const WeatherData = mongoose.model("WeatherData", weatherDataSchema);
 
 // Define a route for the root URL
-app.get('/', (req, res) => {
-    res.render('index');
-  });
-  
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Handle form submission
-app.post('/weather', async (req, res) => {
-    const location = req.body.location;
-    const apiKey = 'jWxJbh6a3rRicgoHSDAejpqN1U7XTh07'; // Get an API key from OpenWeatherMap
-    const encodedLoc = encodeURIComponent(req.body.location);
-    // Read location and encode
+app.post("/weather", async (req, res) => {
+  const location = req.body.location;
+  const apiKey = "jWxJbh6a3rRicgoHSDAejpqN1U7XTh07"; // Get an API key from OpenWeatherMap
+  const encodedLoc = encodeURIComponent(req.body.location);
+  // Read location and encode
 
-    try {
-        const options = {method: 'GET', headers: {accept: 'application/json'}}
-        const response = await fetch(
-        `https://api.tomorrow.io/v4/weather/forecast?location=${encodedLoc}&timesteps=1d&apikey=${apiKey}`, options)
-        const json = await response.json()
-  
-        const weatherData = new WeatherData({
-            location: json.location.name,
-            temperature: json.timelines.daily[0].values.temperatureAvg,
-            humidity: json.timelines.daily[0].values.humidityAvg,
-        });
-        console.log(weatherData)
-        await weatherData.save();
-        let { location, temperature, humidity } = weatherData
-        res.render('weather', { location, temperature, humidity });
-    } catch (error) {
-      console.error(error);
-      res.send('Error fetching weather data.');
-    }
-  });
+  try {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+    const response = await fetch(
+      `https://api.tomorrow.io/v4/weather/forecast?location=${encodedLoc}&timesteps=1d&apikey=${apiKey}`,
+      options
+    );
+    const json = await response.json();
 
-app.get("/favorites", async (req, res) =>{
+    const weatherData = new WeatherData({
+      location: json.location.name,
+      temperature: json.timelines.daily[0].values.temperatureAvg,
+      humidity: json.timelines.daily[0].values.humidityAvg,
+    });
+    console.log(weatherData);
+    await weatherData.save();
+    let { location, temperature, humidity } = weatherData;
+    res.render("weather", { location, temperature, humidity });
+  } catch (error) {
+    console.error(error);
+    res.send("Error fetching weather data.");
+  }
+});
+
+app.get("/favorites", async (req, res) => {
   const cursor = Location.find().cursor();
   const array = await cursor.toArray();
-  let location1, location2, location3; 
+  let location1, location2, location3;
 
   if (array.length == 0) {
     location1 = "Save a location as a favorite";
@@ -90,20 +89,24 @@ app.get("/favorites", async (req, res) =>{
     // Idk lol add the locations if theres enough
   }
 
-  res.render("favorites", { location1, location2, location3 })
-})
-  
+  res.render("favorites", { location1, location2, location3 });
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
 async function favorite(location) {
   const fav = new Location({
-    location: location
+    location: location,
   });
 
   await fav.save();
 }
+
+app.post("/favorite", async (req, res) => {
+  const location = req.body.location;
+  await favorite(location);
+  res.send("Location saved as favorite.");
+});
